177654a6df27751735bb234fae9cfeba2fcb8769
Ilyas022 <ilya.shchetko@mail.ru>
2024-05-21 12:24:03 +0300

    feat: configure storybook

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,7 +1,9 @@
+import React from 'react'
+
 import { CalendarItem } from './styled'
 
-function Calendar() {
-	return <CalendarItem />
+function Calendar({ text }: { text: string }) {
+	return <CalendarItem>{text}</CalendarItem>
 }
 
 export default Calendar
